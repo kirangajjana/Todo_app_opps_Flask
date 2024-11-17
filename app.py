7a5b976cb4d53186c7218facfda8c19d7e940038
kirangajjana <kiran.gajjana@gmail.com>
2024-11-17 22:25:20 +0530

    stacked few updaes

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask,render_template,url_for
 app=Flask(__name__)
 
 @app.route("/home")
-def home():
-    return render_template("home.html",title=home)
+def homepage():
+    return render_template("home.html",title="home")
 
 
 if __name__=="__main__":
