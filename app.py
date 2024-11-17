a3b82d44da5f01140ae51beb9f1faafa0d2e6474
kirangajjana <kiran.gajjana@gmail.com>
2024-11-17 22:19:51 +0530

    updating application.py file

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask,render_template,url_for
 
 app=Flask(__name__)
 
 @app.route("/home")
 def home():
-    return "<h1>hello world</h1>"
+    return render_template("home.html",title=home)
 
 
 if __name__=="__main__":
