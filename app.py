22f6409d491e420f313578b051cc83c6559d80d2
kirangajjana <kiran.gajjana@gmail.com>
2024-11-18 20:58:07 +0530

    updates in app.py

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ from flask import Flask,render_template,url_for
 
 app=Flask(__name__)
 
-@app.route("/home")
-def homepage():
-    return render_template("home.html",title="home")
+@app.route("/home/<int:age>")
+def homepage(age):
+    if age>=20:
+        return render_template("home.html",title="home")
+    else:
+        return "<h1>hello all welocme to the page</h1>"
 
 
 if __name__=="__main__":
